16ea1af342ac07122a4d1eb57796777503c6c0a2
Shahriar Nasim Nafi <shahriarnasim.nafi@gmail.com>
2024-06-30 23:30:44 +0600

    Authorization serverside done

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -24,7 +24,12 @@ func (app *application) routes() http.Handler {
 	router.Handler(http.MethodGet, "/kode/create", dynamic.ThenFunc(app.kodeCreate))
 	router.Handler(http.MethodPost, "/kode/create", dynamic.ThenFunc(app.kodeCreatePost))
 
-	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
+	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
+	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
+	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
+	router.Handler(http.MethodPost, "/user/logout", dynamic.ThenFunc(app.userLogoutPost))
 
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 	return standard.Then(router)
 }
--- internal/models/errors.go
+++ internal/models/errors.go
@@ -3,3 +3,5 @@ package models
 import "errors"
 
 var ErrNoRecord = errors.New("no matching record found")
+var ErrInvalidCredentials = errors.New("invalid credentials")
+var ErrDuplicateEmail = errors.New("duplicate email")
